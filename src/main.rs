d641806d0754354e19c9eebeb5226b0a6056f261
jorge <jorgefco_rf@hotmail.com>
2023-04-27 19:45:35 -0700

    changed fping to ping

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,56 @@
 use ipnet::Ipv4Net;
 use std::sync::{Mutex, Arc};
+use threadpool::ThreadPool;
 
 fn main() {
 
-    let net: Ipv4Net = "192.168.100.0/16".parse().unwrap();
-    let mut threads = vec![];
+    //TODO: Get CLI args
+    //TODO: Get JSON config file
+    let net: Ipv4Net = "10.0.0.0/21".parse().unwrap();
     let active_hosts = Arc::new(Mutex::new(0));
+    let unactive_ips = Arc::new(Mutex::new(vec![""]));
+    let pool = ThreadPool::new(1024);
 
     for host in net.hosts() {
 
         let active_hosts = Arc::clone(&active_hosts);
+        let unactive_ips = Arc::clone(&unactive_ips);
 
-        let t = std::thread::spawn(move || {
+        pool.execute(move || {
 
-            let ip = &host.to_string();
+            let ip = host.to_string();
 
-            match ping(ip) {
+            match ping(&ip) {
                 Ok(true) => {
                     println!("{ip} is up.");
                     let mut count = active_hosts.lock().unwrap();
                     *count += 1;
                 }
-                Ok(false) | Err(_) => {}
+                Ok(false) => {
+                    let mut unactives = unactive_ips.lock().unwrap();
+
+                }
+                
+                Err(_) => {}
             }
         });
-        threads.push(t);
     }
 
-    for t in threads {
-        t.join().unwrap();
-    }
+    pool.join();
 
     let count = active_hosts.lock().unwrap();
     println!("[+] There are {count} active hosts.");
 }
 
 
-fn ping(ip: &str) -> Result<bool, String> {
+
+fn fping(ip: &str) -> Result<bool, String> {
+
     let status = std::process::Command::new("fping")
-        .arg("-t 1000")
+        .arg("-t 20000")
         .arg("-q")
-        .arg("-r 0")
+        .arg("-r 10")
+        .arg("-c 10")
         .arg(ip)
         .status()
         .map_err(|e| e.to_string())?;
@@ -50,5 +60,24 @@ fn ping(ip: &str) -> Result<bool, String> {
     } else {
         Ok(false)
     }
+
+}
+
+fn ping<'a>(ip: &'a str) -> Result<bool, String> {
+
+    let status = std::process::Command::new("ping")
+        .arg("-W 30")
+        .arg("-c 10")
+        .arg("-t 255")
+        .arg(ip)
+        .status()
+        .map_err(|e| e.to_string())?;
+    
+    if status.success(){
+        Ok(true)
+    } else {
+        Ok(false)
+    }
 } 
+
 
